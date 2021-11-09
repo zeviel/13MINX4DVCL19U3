57b7810127f3706e95da01da59338e30af1dda30
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2021-11-09 21:52:54 +0600

    Create menu_configs.py

---
+++ configs/menu_configs.py
@@ -0,0 +1,14 @@
+
+main_menu = [
+    ["Menu", "AminoAdvClique"],
+    ["1", "Global Advertise"],
+    ["2", "Community Advertise"],
+    ["3", "Advertise To Selected User"]
+]
+
+
+additional_menu = [
+    ["Menu", "AminoAdvClique"],
+    ["1", "Using One Account"],
+    ["2", "Using Multiple Accounts"]
+]
