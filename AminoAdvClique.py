6a65dd5a358937c6f09535915c89f6b1f824adc2
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2022-02-26 14:25:00 +0600

    Update AminoAdvClique.py

--- AminoAdvClique.py
+++ AminoAdvClique.py
@@ -1,9 +1,8 @@
-import asyncio
-import pyfiglet
-from sty import fg; print(fg(221))
+from pyfiglet import figlet_format
+from asyncio import get_event_loop
 from configs import main_functions
 print("""Script by DeLuvSushi
 Github : https://github.com/deluvsushi""")
-print(pyfiglet.figlet_format("aminoadvclique", font="slant"))
+print(figlet_format("aminoadvclique", font="slant"))
 
-asyncio.get_event_loop().run_until_complete(main_functions.main())
+get_event_loop().run_until_complete(main_functions.main())
