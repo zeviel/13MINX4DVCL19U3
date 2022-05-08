a14f714ee75b734420c2c263c6ef256901c7e65f
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2022-05-08 22:16:38 +0800

    Update and rename AminoAdvClique.py to 13MINX4DVCL19U3.py

--- 13MINX4DVCL19U3.py
+++ 13MINX4DVCL19U3.py
@@ -3,6 +3,6 @@ from asyncio import get_event_loop
 from configs import main_functions
 print("""Script by DeLuvSushi
 Github : https://github.com/deluvsushi""")
-print(figlet_format("aminoadvclique", font="slant"))
+print(figlet_format("13MINX4DVCL19U3", font="slant"))
 
 get_event_loop().run_until_complete(main_functions.main())
