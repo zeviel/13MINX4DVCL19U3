b391f0c2c35b930ff13a34a1014f43a792b04ee9
zeviel <77536370+zeviel@users.noreply.github.com>
2022-08-25 15:36:55 +0800

    Update 13MINX4DVCL19U3.py

--- 13MINX4DVCL19U3.py
+++ 13MINX4DVCL19U3.py
@@ -1,8 +1,8 @@
 from pyfiglet import figlet_format
 from asyncio import get_event_loop
 from configs import main_functions
-print("""Script by DeLuvSushi
-Github : https://github.com/deluvsushi""")
+print("""Script by zeviel
+Github : https://github.com/zeviel""")
 print(figlet_format("13MINX4DVCL19U3", font="slant"))
 
 get_event_loop().run_until_complete(main_functions.main())
