c539b68cfa5ceb50ea59636b685d59fe7a19a62d
zeviel <77536370+zeviel@users.noreply.github.com>
2022-09-01 18:27:13 +0500

    Update and rename configs/menu_configs.py to utils/configs.py

--- configs/menu_configs.py
+++
@@ -1,7 +0,0 @@
-
-main_menu = [
-    ["Menu", "13MINX4DVCL19U3"],
-    [1, "advertise in global"],
-    [2, "advertise in community"],
-    [3, "advertise to user"]
-]
---
+++ utils/configs.py
@@ -0,0 +1,19 @@
+
+MAIN_MENU = [
+    ["Menu", "13MINX4DVCL19U3"],
+    [1, "advertise in global"],
+    [2, "advertise in community"],
+    [3, "advertise to user"]
+]
+
+LOGO = """ 
+\033[38;5;73m                                                                                                                  
+ #  ### # # ### ### # # # # ##  # #  ## #    #  ### # # ### 
+##    # ###  #  # # # # # # # # # # #   #   ##  # # # #   # 
+ #   ## ###  #  # #  #  ### # # # # #   #    #  ### # #  ## 
+ #    # # #  #  # # # #   # # # # # #   #    #    # # #   # 
+### ### # # ### # # # #   # ##   #   ## ### ### ### ### ### 
+[Author]::: zeviel
+[GitHub]::: https://github.com/zeviel 
+"""
+
